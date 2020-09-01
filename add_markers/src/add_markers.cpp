b649db9963f3fc119ae580f6851f3d1679a257e4
Diego <diego.enrique.ramos@googlemail.com>
2020-09-01 18:38:10 +0200

    feat: Add a queue of objects for the robot to carry.
    Via service requests, the robot asks for more tasks (objects to carry to a storage room) until there are no more objects to be transported.

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,70 +1,119 @@
+#include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <string>
+#include <tuple>
+#include <vector>
 #include <visualization_msgs/Marker.h>
+
+#include "pick_objects/MissionRequest.h"
 
 class MarkerVisualizer{
     public:
-        visualization_msgs::Marker object_marker;
+        //visualization_msgs::Marker object_marker;
+        int current_marker = 0;
+  
+        std::vector<visualization_msgs::Marker> object_markers;
+        std::vector<std::tuple<double, double, double>> pickup_positions = {std::make_tuple(3.61, -4.68, 0.697),
+                                                                            std::make_tuple(-3.443, 2.721, 0.686),
+                                                                            std::make_tuple(-2.282, -5.223, -0.043)};
+
+        std::vector<std::tuple<double, double, double>> dropoff_positions = {std::make_tuple(-7.161, -0.337, -0.003),
+                                                                             std::make_tuple(-6.608, -0.378, -0.003),
+                                                                             std::make_tuple(-6.16, -0.28, 0.013)};
+  
+        bool assign_mission_clbk(pick_objects::MissionRequest::Request& request, 
+                                    pick_objects::MissionRequest::Response& response){
+            ROS_INFO("[MarkerVisualizer] A new mission has been requested.");
+            if(current_marker >= pickup_positions.size()){
+                ROS_INFO("[MarkerVisualizer] There are no more missions to assign.");
+                response.granted.data = false;
+            }else{
+                ROS_INFO("[MarkerVisualizer] A new mission has been assigned.");
+                response.granted.data = true;
+              
+                response.pickup_pose.position.x = std::get<0>(pickup_positions[current_marker]);
+                response.pickup_pose.position.y = std::get<1>(pickup_positions[current_marker]);
+                response.pickup_pose.orientation.w = std::get<2>(pickup_positions[current_marker]);
+              
+                response.dropoff_pose.position.x = std::get<0>(dropoff_positions[current_marker]);
+                response.dropoff_pose.position.y = std::get<1>(dropoff_positions[current_marker]);
+                response.dropoff_pose.orientation.w = std::get<2>(dropoff_positions[current_marker]);         
+            }
+            return true;
+        }
   
         void init_communications(){
-            marker_publisher_ = node_handle_.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-          
+            marker_publisher_ = node_handle_.advertise<visualization_msgs::Marker>("visualization_marker", 10);
             while(marker_publisher_.getNumSubscribers() < 1){
-                ROS_WARN_ONCE("Please create a subscriber to the marker");
+                ROS_WARN_ONCE("[MarkerVisualizer] Please create a subscriber to the marker.");
                 sleep(1.0);
             }
-            ROS_INFO("A Subscriber to the marker has been created.");
-            marker_publisher_.publish(object_marker);
+            ROS_INFO("[MarkerVisualizer] A Subscriber to the marker has been created.");
+            for(visualization_msgs::Marker object_marker : object_markers)
+                marker_publisher_.publish(object_marker);
           
             pickup_subscriber_ = node_handle_.subscribe("/pickup", 10, &MarkerVisualizer::pickup_clbk, this);
             dropoff_subscriber_ = node_handle_.subscribe("/dropoff", 10, &MarkerVisualizer::dropoff_clbk, this);
+          
+            mission_server_ = node_handle_.advertiseService("/request_mission", &MarkerVisualizer::assign_mission_clbk, this);
         }
   
         void init_marker(){
-            uint32_t shape = visualization_msgs::Marker::CUBE;
+            int id_count = 0;
 
-            object_marker.header.frame_id = "/map";
-            object_marker.header.stamp = ros::Time::now();
+            for(std::tuple<double, double, double> position : pickup_positions){
+                visualization_msgs::Marker object_marker;    
+                uint32_t shape = visualization_msgs::Marker::CUBE;
 
-            object_marker.ns = "objects";
-            object_marker.id = 0;
+                object_marker.header.frame_id = "/map";
+                object_marker.header.stamp = ros::Time::now();
 
-            object_marker.type = shape;
+                object_marker.ns = "objects";
+                object_marker.id = id_count;
+                id_count++;
 
-            object_marker.scale.x = 0.3;
-            object_marker.scale.y = 0.3;
-            object_marker.scale.z = 0.3;
+                object_marker.type = shape;
 
-            object_marker.color.r = 0.8f;
-            object_marker.color.g = 0.0f;
-            object_marker.color.b = 0.8f;
-            object_marker.color.a = 1.0;
+                object_marker.scale.x = 0.3;
+                object_marker.scale.y = 0.3;
+                object_marker.scale.z = 0.3;
 
-            object_marker.lifetime = ros::Duration();
+                object_marker.color.r = 0.8f;
+                object_marker.color.g = 0.0f;
+                object_marker.color.b = 0.8f;
+                object_marker.color.a = 1.0;
 
-            object_marker.action = visualization_msgs::Marker::ADD;
+                object_marker.lifetime = ros::Duration();
 
-            object_marker.pose.position.x = 3.61;
-            object_marker.pose.position.y = -4.68;
-            object_marker.pose.position.z = 0;
-            object_marker.pose.orientation.x = 0.0;
-            object_marker.pose.orientation.y = 0.0;
-            object_marker.pose.orientation.z = 0.0;
-            object_marker.pose.orientation.w = 1.0;
+                object_marker.action = visualization_msgs::Marker::ADD;
+
+                object_marker.pose.position.x = std::get<0>(position);
+                object_marker.pose.position.y = std::get<1>(position);
+                object_marker.pose.position.z = 0;
+                object_marker.pose.orientation.x = 0.0;
+                object_marker.pose.orientation.y = 0.0;
+                object_marker.pose.orientation.z = 0.0;
+                object_marker.pose.orientation.w = 1.0;
+
+                object_markers.push_back(object_marker);
+            }
+
         }
   
         void pickup_clbk(const std_msgs::Bool notification){
-            ROS_INFO("Picking up the object!");
-            object_marker.action = visualization_msgs::Marker::DELETE;
-            marker_publisher_.publish(object_marker);
+            ROS_INFO("[MarkerVisualizer] Picking up the object!");
+            object_markers[current_marker].action = visualization_msgs::Marker::DELETE;
+            marker_publisher_.publish(object_markers[current_marker]);
         }
   
         void dropoff_clbk(const std_msgs::Bool notification){
-            ROS_INFO("Dropping off the object!");
-            object_marker.action = visualization_msgs::Marker::ADD;
-            object_marker.pose.position.x = -6.16;
-            object_marker.pose.position.y = -0.28;
-            marker_publisher_.publish(object_marker);
+            ROS_INFO("[MarkerVisualizer] Dropping off the object!");
+            object_markers[current_marker].action = visualization_msgs::Marker::ADD;
+            object_markers[current_marker].pose.position.x = std::get<0>(dropoff_positions[current_marker]);
+            object_markers[current_marker].pose.position.y = std::get<1>(dropoff_positions[current_marker]);
+            marker_publisher_.publish(object_markers[current_marker]);
+            current_marker++;
         }
   
     private:
@@ -72,11 +121,12 @@ class MarkerVisualizer{
         ros::Publisher marker_publisher_; 
         ros::Subscriber pickup_subscriber_;
         ros::Subscriber dropoff_subscriber_;
+        ros::ServiceServer mission_server_;
 };
 
 int main( int argc, char** argv ){
     ros::init(argc, argv, "add_markers");
-
+    ROS_INFO("[MarkerVisualizer] Running!");
     MarkerVisualizer visualizer;
     visualizer.init_marker();
     visualizer.init_communications();
--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -1,89 +1,141 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include "pick_objects/MissionRequest.h"
 #include <actionlib/client/simple_action_client.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+class ObjectPicker{
+    public:
+        actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> * move_base_client = NULL;
+
+        move_base_msgs::MoveBaseGoal current_pickup_position;
+        move_base_msgs::MoveBaseGoal current_dropoff_position;
+        move_base_msgs::MoveBaseGoal home;
+
+        ros::Publisher pickup_publisher_;
+        ros::Publisher dropoff_publisher_;
+        ros::ServiceClient request_mission_client_;
+
+    private:
+        ros::NodeHandle node_handle_;        
+        
+    public:
+        void init_communications(){
+            home.target_pose.header.frame_id = "map";
+            home.target_pose.header.stamp = ros::Time::now();
+
+            home.target_pose.pose.position.x = -0.738;
+            home.target_pose.pose.position.y = -1.038;
+            home.target_pose.pose.orientation.w = 1.0;
+
+            move_base_client = new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>(node_handle_, "move_base", true);
+            pickup_publisher_ = node_handle_.advertise<std_msgs::Bool>("/pickup", 10);
+            dropoff_publisher_ = node_handle_.advertise<std_msgs::Bool>("/dropoff", 10);
+            request_mission_client_ = node_handle_.serviceClient<pick_objects::MissionRequest>("/request_mission");
+        }
+
+        bool request_mission(){
+            bool response = true;
+            pick_objects::MissionRequest service_request;
+            service_request.request.request.data = true;
+            if(request_mission_client_.call(service_request)){
+                ROS_INFO("[ObjectPicker] Call to service succeded!");
+                if(service_request.response.granted.data == true){
+                    current_pickup_position.target_pose.header.frame_id = "map";
+                    current_pickup_position.target_pose.header.stamp = ros::Time::now();
+
+                    current_pickup_position.target_pose.pose.position.x = service_request.response.pickup_pose.position.x;
+                    current_pickup_position.target_pose.pose.position.y = service_request.response.pickup_pose.position.y;
+                    current_pickup_position.target_pose.pose.orientation.w = service_request.response.pickup_pose.orientation.w;
+
+		    current_dropoff_position.target_pose.header.frame_id = "map";
+                    current_dropoff_position.target_pose.header.stamp = ros::Time::now();
+
+                    current_dropoff_position.target_pose.pose.position.x = service_request.response.dropoff_pose.position.x;
+                    current_dropoff_position.target_pose.pose.position.y = service_request.response.dropoff_pose.position.y;
+                    current_dropoff_position.target_pose.pose.orientation.w = service_request.response.dropoff_pose.orientation.w;
+                }else{
+                    response = false;
+                }
+            }else{
+                ROS_INFO("[ObjectPicker] Call to service failed!");
+            }
+            return response;
+        }
+
+};
+
+
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "pick_objects");
-
-    MoveBaseClient ac("move_base", true);
+    ROS_INFO("[ObjectPicker] Running!");
+    	
+    ObjectPicker picker;
+    picker.init_communications();
     
-    ros::NodeHandle node_handle;
-    ros::Publisher pickup_publisher = node_handle.advertise<std_msgs::Bool>("/pickup", 10);
-    ros::Publisher dropoff_publisher = node_handle.advertise<std_msgs::Bool>("/dropoff", 10);
-
-    while(!ac.waitForServer(ros::Duration(5.0))){
-        ROS_INFO("Waiting for the move_base action server...");
+    while(!picker.move_base_client->waitForServer(ros::Duration(5.0))){
+        ROS_INFO("[ObjectPicker] Waiting for the move_base action server...");
+    }
+    
+    while(!picker.request_mission_client_.waitForExistence()){
+        ROS_INFO("[ObjectPicker] Waiting for the mission server...");
     }
 
-    move_base_msgs::MoveBaseGoal goal_pickup_zone;
-    move_base_msgs::MoveBaseGoal goal_dropoff_zone;
-    move_base_msgs::MoveBaseGoal goal_home;
+    ros::Rate rate(10);
+    while(ros::ok()){
+        bool service_response = picker.request_mission();
+        if(service_response){
+            ROS_INFO("[ObjectPicker] A new mission has been received.");
+            picker.current_pickup_position.target_pose.header.stamp = ros::Time::now();
+            picker.move_base_client->sendGoal(picker.current_pickup_position);
+            picker.move_base_client->waitForResult();
 
-    goal_pickup_zone.target_pose.header.frame_id = "map";
-    goal_pickup_zone.target_pose.header.stamp = ros::Time::now();
+            if(picker.move_base_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+                ROS_INFO("[ObjectPicker] The pickup zone has been reached, picking up object...");
+                std_msgs::Bool pickup_notification;
+                pickup_notification.data = true;
+                picker.pickup_publisher_.publish(pickup_notification);
+            }else{
+                ROS_INFO("[ObjectPicker] The robot failed to reach the pickup zone...");
+            }
+            ros::Duration(5.0).sleep();
 
-    goal_pickup_zone.target_pose.pose.position.x = 3.61;
-    goal_pickup_zone.target_pose.pose.position.y = -4.68;
-    goal_pickup_zone.target_pose.pose.orientation.w = 0.697;
+            ROS_INFO("[ObjectPicker] Moving towards dropoff zone.");
+            picker.current_dropoff_position.target_pose.header.stamp = ros::Time::now();
+            picker.move_base_client->sendGoal(picker.current_dropoff_position);
+            picker.move_base_client->waitForResult();
 
-    goal_dropoff_zone.target_pose.header.frame_id = "map";
-    goal_dropoff_zone.target_pose.header.stamp = ros::Time::now();
+            if(picker.move_base_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+                ROS_INFO("[ObjectPicker] The dropoff zone has been reached, dropping off the object...");
+                std_msgs::Bool dropoff_notification;
+                dropoff_notification.data = true;
+                picker.dropoff_publisher_.publish(dropoff_notification);
+                ros::Duration(5.0).sleep();
+            }else{
+                ROS_INFO("[ObjectPicker] The robot failed to reach the dropoff zone...");
+            }
+        }else{
+            ROS_INFO("[ObjectPicker] There are no more missions, going back home: (%f, %f, %f)", 
+                                     picker.home.target_pose.pose.position.x,
+                                     picker.home.target_pose.pose.position.y,
+                                     picker.home.target_pose.pose.orientation.w);
+            picker.home.target_pose.header.stamp = ros::Time::now();
+            picker.move_base_client->sendGoal(picker.home);
+            picker.move_base_client->waitForResult();
 
-    goal_dropoff_zone.target_pose.pose.position.x = -6.16;
-    goal_dropoff_zone.target_pose.pose.position.y = -0.28;
-    goal_dropoff_zone.target_pose.pose.orientation.w = 0.013;
-
-    goal_home.target_pose.header.frame_id = "map";
-    goal_home.target_pose.header.stamp = ros::Time::now();
-
-    goal_home.target_pose.pose.position.x = -0.48;
-    goal_home.target_pose.pose.position.y = -0.8;
-    goal_home.target_pose.pose.orientation.w = 1.0;
-
-    ROS_INFO("Moving towards pickup zone.");
-    ac.sendGoal(goal_pickup_zone);
-
-    ac.waitForResult();
-
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-        ROS_INFO("The pickup zone has been reached!");
-        std_msgs::Bool pickup_notification;
-        pickup_notification.data = true;
-        pickup_publisher.publish(pickup_notification);
-    }else{
-        ROS_INFO("The robot failed to reach the pickup zone...");
+            if(picker.move_base_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+                ROS_INFO("[ObjectPicker] The robot reached home!");
+            }else{
+                ROS_WARN("[ObjectPicker] The robot failed to reach home!");
+            }
+            ros::Duration(5.0);
+            ros::shutdown();
+        }
+        rate.sleep();
     }
-    ros::Duration(5.0).sleep();
-
-    ROS_INFO("Moving towards dropoff zone.");
-    ac.sendGoal(goal_dropoff_zone);
-
-    ac.waitForResult();
-
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-        ROS_INFO("The dropoff zone has been reached!");
-        std_msgs::Bool dropoff_notification;
-        dropoff_notification.data = true;
-        dropoff_publisher.publish(dropoff_notification);
-        ros::Duration(5.0).sleep();
-        ROS_INFO("Mission accomplished, going back home!");
-    }else{
-        ROS_INFO("The robot failed to reach the dropoff zone...");
-    }
-
-    ac.sendGoal(goal_home);
-
-    ac.waitForResult();
-
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-        ROS_INFO("The robot reached home!");
-    }else{
-        ROS_WARN("The robot failed to reach home!");
-    }
-    ros::Duration(5.0);
 
     return 0;
 }
